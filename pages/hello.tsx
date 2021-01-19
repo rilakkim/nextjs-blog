5eee285fdaac5f21156823c924a5cf4d6286cee0
Hyungjun Kim <hyungjun.kim@Hyungjuns-MBP.fritz.box>
2021-01-19 17:22:10 +0100

    create nextjs blog

---
+++ pages/hello.tsx
@@ -0,0 +1,10 @@
+import { Article } from '@components/Article'
+
+export default function Hello() {
+    return (
+        <Article>
+            <h1>Hello</h1>
+            <p>This is Hello Page</p>
+        </Article>
+    )
+}
